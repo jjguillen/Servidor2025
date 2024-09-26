7d7bec0d521434b732eb4d09df653f3675f566cf
jjguillen <jjavierguillen@gmail.com>
2024-09-26 17:58:20 +0200

    jueves

--- php/index.php
+++ php/index.php
@@ -41,11 +41,51 @@
         echo "<br><a href='http://www.marca.com'>Link</a>";
         echo '<br><a href="http://www.google.es">Link</a><br>';
 
+        $frutas = array("pera", "manzana", "plátano");
+        $numeros = [1,2,3,4];
+
+        for($i=0; $i<count($numeros) ;$i++) {
+            echo $numeros[$i]."<br>";
+        }
+
+        echo "<br>". $frutas[1];
+
+        $frutas[5] = "melón";
+        $frutas[6] = "naranja";
+
+        echo "frutas<br>";
+        foreach($frutas as $fruta) {
+            echo $fruta."<br>";
+        }
+
+        //Arrays asociativos
+        $persona1 = array("nombre" => "Juan", "edad" => 25, "sexo" => "H");
+        $persona2 = array("nombre" => "Pepe", "edad" => 30, "sexo" => "H");
+        
+        $personas = array($persona1, $persona2);
+
+        foreach($personas as $persona) {
+            foreach($persona as $valor) {
+                echo $valor . " ";
+            }
+            echo "<br>";
+        }
+
+        foreach($persona1 as $clave => $valor) {
+            echo $clave . " -> " . $valor . "<br>";
+        }
+        
+
+
+
+
+
+
 
 
     ?>
 
-    <a href="http://localhost:8080/otro.php">Otro php</a>
+    <a href="http://localhost:8080/otro.php?id=3&nombre=pepe">Otro php</a>
 
     <?= "<br>Esto es más rápido aún" ?>
 
--- php/otro.php
+++ php/otro.php
@@ -1,3 +1,10 @@
 <?php
-    echo "Soy otro";
+    if (isset($_GET['id'])) {
+        echo "ID: " . $_GET['id'];
+    }
+
+    echo "<br>";
+    //empty significa que tiene valor vacío, no nulo
+    echo empty($_GET['nombre']) ? "Está vacío" : $_GET['nombre'];
+
 ?>
