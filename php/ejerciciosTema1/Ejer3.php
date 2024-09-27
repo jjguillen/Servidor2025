11eaf5f685309191255e506207997aa1df4dbcbf
jjguillen <jjavierguillen@gmail.com>
2024-09-27 17:54:13 +0200

    ejercicios clase

---
+++ php/ejerciciosTema1/Ejer3.php
@@ -0,0 +1,8 @@
+<?php
+
+    $radio = rand(1, 1000);
+
+    $volumen = (4 / 3) * M_PI * pow($radio, 3);
+
+    echo "El volumen del círculo con radio $radio es $volumen";
+?>
---
+++ php/ejerciciosTema1/Ejer8.php
@@ -0,0 +1,22 @@
+<?php
+
+function pintar($array) {
+    echo "<br>";
+    foreach($array as $valor) {
+        echo " ".$valor;
+    }
+}
+
+$comb = array();
+for($i=0; $i<6; $i++) {
+    do {
+        $num = rand(1,49);
+    } while( in_array($num, $comb) );
+    $comb[] = $num;
+}
+
+sort($comb);
+pintar($comb);
+
+
+?>
